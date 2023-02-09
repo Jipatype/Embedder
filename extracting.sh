235b22185a59f297204f2851cd7563cc194c4617
Jipatype <jipatype@gmail.com>
2023-02-09 08:28:07 +0700

    Update extracting.sh

--- extracting.sh
+++ extracting.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-cd "$(dirname "$0")"
 extractor="PATH/TO/EXTRACTOR"
+font_input_folder="/PATH/TO/FONT_INPUT_FOLDER"
 file_output_folder="PATH/TO/OUTPUT_FOLDER"
 mkdir -p "$file_output_folder"
-for file in *.otf; do 
+for file in "$font_input_folder/"*.otf; do
     file_name $(basename -a $file)
     "$extractor" -i "$file" -o "$file_output_folder/$file_name.txt"
 done
